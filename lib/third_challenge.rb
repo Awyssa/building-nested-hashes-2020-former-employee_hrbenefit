93a05b154c19127ad7566794a63fe1d65376ad55
Michael Ernest <m.hendersun@gmail.com>
2020-10-07 19:17:28 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,4 +1,6 @@
-epic_tragedy = {
+def third_challenge
+
+  epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -8,9 +10,8 @@ epic_tragedy = {
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine => {name: "Juliette", age: "15", status: "alive"},
       :heroine_friends => []
    }
-}
-
- 
+  }
+end
